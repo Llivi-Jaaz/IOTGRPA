b67234e20414945a08ac7e7b1cbdf54c2b96a10c
Justine Marie Dusaran <jdusaran05@gmail.com>
2023-11-23 14:55:05 +0800

    Update forecast page

--- src/sections/forecast/forecast-data.jsx
+++ src/sections/forecast/forecast-data.jsx
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
 import CardHeader from '@mui/material/CardHeader';
 
 import Chart, { useChart } from 'src/components/chart';
 
-// ----------------------------------------------------------------------
-
-export default function TempData({ title, subheader, chart, ...other }) {
-  const { labels, colors, series, options } = chart;
+export default function TempData({ title, subheader, chart, onChartTypeChange, ...other }) {
+  const { labels, colors, series, options, xaxisLabel } = chart;
 
   const chartOptions = useChart({
     colors,
@@ -25,12 +24,12 @@ export default function TempData({ title, subheader, chart, ...other }) {
     xaxis: {
       type: 'time',
       title: {
-        text: 'Hours', 
+        text: xaxisLabel,
       },
     },
     yaxis: {
       title: {
-        text: 'Temperature Value',
+        text: 'Temperature',
       },
     },
     tooltip: {
@@ -52,7 +51,10 @@ export default function TempData({ title, subheader, chart, ...other }) {
     <Card {...other}>
       <CardHeader title={title} subheader={subheader} />
 
-      <Box sx={{ p: 3, pb: 1 }}>
+      <Box sx={{ p: 3, pb: 1}}>
+        <Button onClick={() => onChartTypeChange('1week')} sx={{ ml: 120, display: 'inline' }}>1 Week</Button>
+        <Button onClick={() => onChartTypeChange('24hrs')} sx={{ ml: 'auto', display: 'inline' }}>24 Hrs</Button>
+
         <Chart
           dir="ltr"
           type="line"
@@ -70,4 +72,5 @@ TempData.propTypes = {
   chart: PropTypes.object,
   subheader: PropTypes.string,
   title: PropTypes.string,
+  onChartTypeChange: PropTypes.func,
 };
--- src/sections/forecast/view/forecast.jsx
+++ src/sections/forecast/view/forecast.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import React, { useState } from 'react';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -9,8 +10,12 @@ import TempData from '../forecast-data';
 // ----------------------------------------------------------------------
 
 export default function ForecastView() {
+  const currentDate = moment().format('dddd, MMMM DD, YYYY');
+  const [chartType, setChartType] = useState('1week'); // Default chart type
 
-  const currentDate = moment().format('dddd, MMMM DD, YYYY');  
+  const handleChartTypeChange = (type) => {
+    setChartType(type);
+  };
 
   return (
     <Container>
@@ -21,36 +26,48 @@ export default function ForecastView() {
         Today is {currentDate}
       </Typography>
 
-      <Grid xs={12} md={8} lg={8} sx={{ mt: 4 }}>
-          <TempData
-            title="Temperature"
-            subheader="Short-term Forecast"
-            chart={{
-              labels: [
-                '0',
-                '2',
-                '4',
-                '6',
-                '8',
-                '10',
-                '12',
-                '14',
-                '16',
-                '18',
-                '20',
-                '22',
-                '24',
-              ],
-              series: [
-                {
-                  type: 'line',
-                  fill: 'solid',
-                  data: [27, 29, 28.5, 30, 31, 30, 31.4, 28.3, 29.8, 26, 27, 27.3, 26.5],
-                }
-              ],
-            }}
-          />
+      <Grid sx={{ mt: 4 }}>
+      <Grid xs={12} md={8} lg={8}>
+          {chartType === '1week' && (
+            <TempData
+              title="Temperature"
+              subheader="Long-term Forecast"
+              chart={{
+                labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                series: [
+                  {
+                    type: 'line',
+                    fill: 'solid',
+                    data: [28, 29.4, 28.5, 30, 29, 30, 28.4],
+                  },
+                ],
+                xaxisLabel: 'Days',
+              }}
+              onChartTypeChange={handleChartTypeChange}
+            />
+          )}
         </Grid>
+        <Grid xs={12} md={8} lg={8}>
+          {chartType === '24hrs' && (
+            <TempData
+              title="Temperature"
+              subheader="Short-term"
+              chart={{
+                labels: ['0', '2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22', '24'],
+                series: [
+                  {
+                    type: 'line',
+                    fill: 'solid',
+                    data: [27, 29, 28.5, 30, 31, 30, 31.4, 28.3, 29.8, 26, 27, 27.3, 26.5],
+                  },
+                ],
+              xaxisLabel: 'Hours',
+              }}
+              onChartTypeChange={handleChartTypeChange}
+            />
+          )}
+        </Grid>        
+      </Grid>
     </Container>
   );
 }
