e54cbf127fb479b3b3140f67da9eaf6d4723a3c6
Ronnie <77873456+veronicabaron224@users.noreply.github.com>
2023-11-30 13:41:19 +0800

    Started fixing location information

--- src/sections/blog/post-card.jsx
+++ src/sections/blog/post-card.jsx
@@ -3,48 +3,20 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
 import { alpha } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 
 import { fDate } from 'src/utils/format-time';
-import { fShortenNumber } from 'src/utils/format-number';
-
-import Iconify from 'src/components/iconify';
-import SvgColor from 'src/components/svg-color';
 
 // ----------------------------------------------------------------------
 
 export default function PostCard({ post, index }) {
-  const { cover, title, view, comment, share, author, createdAt } = post;
+  const { title, createdAt } = post;
 
   const latestPostLarge = index === 0;
 
   const latestPost = index === 1 || index === 2;
-
-  const renderAvatar = (
-    <Avatar
-      alt={author.name}
-      src={author.avatarUrl}
-      sx={{
-        zIndex: 9,
-        width: 32,
-        height: 32,
-        position: 'absolute',
-        left: (theme) => theme.spacing(3),
-        bottom: (theme) => theme.spacing(-2),
-        ...((latestPostLarge || latestPost) && {
-          zIndex: 9,
-          top: 24,
-          left: 24,
-          width: 40,
-          height: 40,
-        }),
-      }}
-    />
-  );
 
   const renderTitle = (
     <Link
@@ -67,54 +39,6 @@ export default function PostCard({ post, index }) {
     </Link>
   );
 
-  const renderInfo = (
-    <Stack
-      direction="row"
-      flexWrap="wrap"
-      spacing={1.5}
-      justifyContent="flex-end"
-      sx={{
-        mt: 3,
-        color: 'text.disabled',
-      }}
-    >
-      {[
-        { number: comment, icon: 'eva:message-circle-fill' },
-        { number: view, icon: 'eva:eye-fill' },
-        { number: share, icon: 'eva:share-fill' },
-      ].map((info, _index) => (
-        <Stack
-          key={_index}
-          direction="row"
-          sx={{
-            ...((latestPostLarge || latestPost) && {
-              opacity: 0.48,
-              color: 'common.white',
-            }),
-          }}
-        >
-          <Iconify width={16} icon={info.icon} sx={{ mr: 0.5 }} />
-          <Typography variant="caption">{fShortenNumber(info.number)}</Typography>
-        </Stack>
-      ))}
-    </Stack>
-  );
-
-  const renderCover = (
-    <Box
-      component="img"
-      alt={title}
-      src={cover}
-      sx={{
-        top: 0,
-        width: 1,
-        height: 1,
-        objectFit: 'cover',
-        position: 'absolute',
-      }}
-    />
-  );
-
   const renderDate = (
     <Typography
       variant="caption"
@@ -130,22 +54,6 @@ export default function PostCard({ post, index }) {
     >
       {fDate(createdAt)}
     </Typography>
-  );
-
-  const renderShape = (
-    <SvgColor
-      color="paper"
-      src="/assets/icons/shape-avatar.svg"
-      sx={{
-        width: 80,
-        height: 36,
-        zIndex: 9,
-        bottom: -15,
-        position: 'absolute',
-        color: 'background.paper',
-        ...((latestPostLarge || latestPost) && { display: 'none' }),
-      }}
-    />
   );
 
   return (
@@ -174,11 +82,7 @@ export default function PostCard({ post, index }) {
             }),
           }}
         >
-          {renderShape}
-
-          {renderAvatar}
-
-          {renderCover}
+          {}
         </Box>
 
         <Box
@@ -194,8 +98,6 @@ export default function PostCard({ post, index }) {
           {renderDate}
 
           {renderTitle}
-
-          {renderInfo}
         </Box>
       </Card>
     </Grid>
