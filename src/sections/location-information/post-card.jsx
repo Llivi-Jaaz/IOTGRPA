3ccc88c42199f0f9fe5dcf115296548f54967151
adrienkcbalane <adrien.kyle.balane@adamson.edu.ph>
2023-12-13 11:53:13 +0800

    save

--- src/sections/location-information/post-card.jsx
+++ src/sections/location-information/post-card.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Card from '@mui/material/Card';
-import { alpha } from '@mui/material/styles';
-import Grid from '@mui/material/Unstable_Grid2';
-import Typography from '@mui/material/Typography';
-
-import { fDate } from 'src/utils/format-time';
 
 // ----------------------------------------------------------------------
 
@@ -16,112 +6,20 @@ const GoogleMap = () => (
   <div className="mapouter">
     <div className="gmap_canvas">
       <iframe
-        src="https://maps.google.com/maps?q=NCR&amp;t=k&amp;z=10&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+        src="https://maps.google.com/maps?q=ncr&amp;t=&amp;z=10&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
         frameBorder="0"
         allowFullScreen
         title="Google Map"
-        style={{ width: '100%', height: '100%' }}
-      ></iframe>
+        style={{ width: '1080px', height: '520px' }}
+      />
+      <style>
+        {`.mapouter{position:relative;height:520px;width:1080px;background:#fff;}
+        .maprouter a{color:#fff !important;position:absolute !important;top:0 !important;z-index:0 !important;}
+        .gmap_canvas{overflow:hidden;height:520px;width:1080px}.gmap_canvas iframe{position:relative;z-index:2}`}
+      </style>
+      <a href="https://blooketjoin.org/">blooket</a>
     </div>
   </div>
 );
 
 export { GoogleMap };
-
-export default function PostCard({ post, index }) {
-  const { title, createdAt } = post;
-
-  const latestPostLarge = index === 0;
-
-  const latestPost = index === 1 || index === 2;
-
-  const renderTitle = (
-    <Link
-      color="inherit"
-      variant="subtitle2"
-      underline="hover"
-      sx={{
-        height: 44,
-        overflow: 'hidden',
-        WebkitLineClamp: 2,
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        ...(latestPostLarge && { typography: 'h5', height: 60 }),
-        ...((latestPostLarge || latestPost) && {
-          color: 'common.white',
-        }),
-      }}
-    >
-      {title}
-    </Link>
-  );
-
-  const renderDate = (
-    <Typography
-      variant="caption"
-      component="div"
-      sx={{
-        mb: 2,
-        color: 'text.disabled',
-        ...((latestPostLarge || latestPost) && {
-          opacity: 0.48,
-          color: 'common.white',
-        }),
-      }}
-    >
-      {fDate(createdAt)}
-    </Typography>
-  );
-
-  return (
-    <Grid xs={12} sm={latestPostLarge ? 12 : 6} md={latestPostLarge ? 6 : 3}>
-      <Card>
-        <Box
-          sx={{
-            position: 'relative',
-            pt: 'calc(100% * 3 / 4)',
-            ...((latestPostLarge || latestPost) && {
-              pt: 'calc(100% * 4 / 3)',
-              '&:after': {
-                top: 0,
-                content: "''",
-                width: '100%',
-                height: '100%',
-                position: 'absolute',
-                bgcolor: (theme) => alpha(theme.palette.grey[900], 0.72),
-              },
-            }),
-            ...(latestPostLarge && {
-              pt: {
-                xs: 'calc(100% * 4 / 3)',
-                sm: 'calc(100% * 3 / 4.66)',
-              },
-            }),
-          }}
-        >
-          {}
-        </Box>
-
-        <Box
-          sx={{
-            p: (theme) => theme.spacing(4, 3, 3, 3),
-            ...((latestPostLarge || latestPost) && {
-              width: 1,
-              bottom: 0,
-              position: 'absolute',
-            }),
-          }}
-        >
-          {renderDate}
-
-          {renderTitle}
-        </Box>
-      </Card>
-    </Grid>
-  );
-}
-
-PostCard.propTypes = {
-  post: PropTypes.object.isRequired,
-  index: PropTypes.number,
-};
