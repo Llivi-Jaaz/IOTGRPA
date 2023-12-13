83a1833f169716dd89c5d59d80803dd98ad7ce51
Llivi-Jaaz <llivicaina@gmail.com>
2023-12-13 12:31:04 +0800

    design

--- src/sections/overview/app-widget-summary.jsx
+++ src/sections/overview/app-widget-summary.jsx
@@ -10,23 +10,24 @@ export default function AppWidgetSummary({ title, subheader, data, total, icon, 
   return (
     <Card
       component={Stack}
-      spacing={3}
+      spacing={4}
       direction="row"
       sx={{
-        px: 3,
-        py: 20,
-        borderRadius: 2,
+        px: 2,
+        py: 15,
+        borderRadius: 3,
         position: 'relative',
+        backgroundColor: '#E5F3F0', // Change the color here
         ...sx,
       }}
       {...other}
     >
       <Stack spacing={0.1} sx={{ position: 'absolute', top: 20, left: 20 }}>
-        <Typography variant="h6">
+        <Typography variant="h6" color='#52796F'>
           {title}
         </Typography>
 
-        <Typography variant="subtitle1">
+        <Typography variant="subtitle1" color='#52796F'>
           {subheader}
         </Typography>
       </Stack>
