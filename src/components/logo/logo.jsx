9a6ac8244b2f2f9f281c7cf43fe6f8b452b8f949
Llivi-Jaaz <llivicaina@gmail.com>
2023-12-15 14:08:19 +0800

    updates

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,29 +1,31 @@
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
-
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-
+import Typography from '@mui/material/Typography';
 import { RouterLink } from 'src/routes/components';
 
-// ----------------------------------------------------------------------
-
 const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
-  const logo = (
-    <Box
-      component="img"
-      src="/favicon/aws_logo.svg"
-      sx={{ width: 30, height: 30, cursor: 'pointer', ...sx }}
-    />
+  const logoContent = (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box
+        component="img"
+        src="/favicon/aws_logo.svg"
+        sx={{ width: 30, height: 30, cursor: 'pointer', ...sx }}
+      />
+      <Typography variant="h3" sx={{ marginLeft: 1, marginTop: 3,  color: '#1EA480'}}>
+        Weather
+      </Typography>
+    </Box>
   );
 
   if (disabledLink) {
-    return logo;
+    return logoContent;
   }
 
   return (
     <Link component={RouterLink} sx={{ display: 'contents' }}>
-      {logo}
+      {logoContent}
     </Link>
   );
 });
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -6,7 +6,7 @@ import {
   UilBrightness,
   UilCloudCheck,
   UilRaindropsAlt,
-  UilTemperaturePlus, 
+  UilTemperaturePlus,
   UilCloudShowersHeavy,
 } from '@iconscout/react-unicons';
 
@@ -89,7 +89,7 @@ export default function AppView() {
         Today is {currentDate}
       </Typography>
 
-      <Grid container spacing={2}>
+      <Grid container spacing={4}>
         <Grid xs={12} sm={6} md={4}>
           <AppWidgetSummary
             icon={<UilTemperaturePlus size={50} />}
